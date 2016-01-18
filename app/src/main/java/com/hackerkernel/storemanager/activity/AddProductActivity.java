001da3dc479b63a3fb72910bc04f05b095b8499b
husainsaify <hackerkernel15@gmail.com>
2016-01-18 16:58:07 +0530

    Add animation on Adding and deleting of size quantity views

--- app/src/main/java/com/hackerkernel/storemanager/activity/AddProductActivity.java
+++ app/src/main/java/com/hackerkernel/storemanager/activity/AddProductActivity.java
@@ -15,6 +15,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.TranslateAnimation;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;

d2eb1fad5a078b306335817d33f0c9f03e881bf3
husainsaify <hackerkernel15@gmail.com>
2016-02-02 00:33:32 +0530

    added activity to edit products

---
+++ app/src/main/java/com/hackerkernel/storemanager/activity/EditProductActivity.java
@@ -0,0 +1,15 @@
+package com.hackerkernel.storemanager.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hackerkernel.storemanager.R;
+
+public class EditProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_product);
+    }
+}

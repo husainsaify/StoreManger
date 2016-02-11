2bed2360c77f019f14cf0c72fdeae40a56ea9707
husainsaify <hackerkernel15@gmail.com>
2016-02-11 15:03:51 +0530

    Created DatePicker For calculate commission

--- app/src/main/java/com/hackerkernel/storemanager/activity/CalculateCommissionActivity.java
+++ app/src/main/java/com/hackerkernel/storemanager/activity/CalculateCommissionActivity.java
@@ -1,13 +1,19 @@
 package com.hackerkernel.storemanager.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 
 import com.hackerkernel.storemanager.R;
+import com.hackerkernel.storemanager.util.DatePicker;
 import com.hackerkernel.storemanager.util.GetSalesman;
+
+import java.util.Calendar;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -16,6 +22,11 @@ public class CalculateCommissionActivity extends AppCompatActivity {
     @Bind(R.id.toolbar) Toolbar mToolbar;
     @Bind(R.id.salesmanSpinner) Spinner mSalesmanSpinner;
     @Bind(R.id.layout) RelativeLayout mLayoutForSnackbar;
+    @Bind(R.id.fromDateButton) Button mFromDateButton;
+    @Bind(R.id.toDateButton) Button mToDateButton;
+
+
+    private String mSalesmanId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +41,37 @@ public class CalculateCommissionActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //Setup salesman spinner
-        GetSalesman getSalesman = new GetSalesman(this,mLayoutForSnackbar,mSalesmanSpinner);
+        final GetSalesman getSalesman = new GetSalesman(this,mLayoutForSnackbar,mSalesmanSpinner);
         getSalesman.setupSalesmanSpinner();
+
+        //when Salesman is selected from spinnner
+        mSalesmanSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mSalesmanId = getSalesman.getSalesman(position).getId();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        mFromDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePicker picker = new DatePicker();
+                picker.show(getSupportFragmentManager(), "fromDate");
+            }
+        });
+
+        mToDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePicker picker = new DatePicker();
+                picker.show(getSupportFragmentManager(),"toDate");
+            }
+        });
     }
 }
